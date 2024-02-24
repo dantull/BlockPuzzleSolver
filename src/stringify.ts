f6f1624234ef41622160860703d5e4c4571be084
Dan Tull <dantull@gmail.com>
2024-02-24 13:51:01 -0600

    Extract conversion routines.

---
+++ src/stringify.ts
@@ -0,0 +1,48 @@
+import { Point, Shape, VisualShape } from "./types";
+
+export function convert_to_points(shape: string[]): Point[] {
+    const points = [];
+    for (var y = 0; y < shape.length; y++) {
+        const line = shape[y];
+        for (var x = 0; x < line.length; x++) {
+            if (line.charAt(x) !== " ") {
+                points.push({x, y});
+            }
+        }
+    }
+
+    return points; 
+}
+
+export function bounds(ps: Point[]): [Point, Point] {
+    const minX = ps.reduce((m:number, p) => Math.min(m, p.x), Number.POSITIVE_INFINITY);
+    const maxX = ps.reduce((m:number, p) => Math.max(m, p.x), Number.NEGATIVE_INFINITY);
+    const minY = ps.reduce((m:number, p) => Math.min(m, p.y), Number.POSITIVE_INFINITY);
+    const maxY = ps.reduce((m:number, p) => Math.max(m, p.y), Number.NEGATIVE_INFINITY);
+    return [{x:minX, y:minY}, {x:maxX, y:maxY}];
+}
+
+export function convert_to_strings(ps:Point[], to_char:(p:Point) => string): string[] {
+    const [min, max] = bounds(ps);
+
+    const grid:string[][] = [];
+    const width = max.x - min.x + 1;
+    const height = max.y - min.y + 1;
+    for (let i = 0; i < height; i++) {
+        grid[i] = new Array(width).fill(' ');
+    }
+
+    for (let p of ps) {
+        grid[p.y - min.y][p.x - min.x] = to_char(p);
+    }
+
+    return grid.map((cs) => cs.join(""));
+}
+
+export function convert_to_shape(vs: VisualShape): Shape {
+    return {
+        chiral: vs.chiral,
+        rotations: vs.rotations,
+        points: convert_to_points(vs.points)
+    }
+}
