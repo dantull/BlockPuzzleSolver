e1a99bad1bb76a3abf524baf08c5e19d59f744d2
Dan Tull <dantull@gmail.com>
2024-03-16 04:22:21 -0500

    Simple but messy Node.js parallel worker impl
    Note this temporarily breaks the browser code, will fix by separating
    these more fully.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 // 10 unique pieces, some of which differ when flipped (chirality)
 // and rotated (usually 4 shapes, but tetra I only has 2)
 
-import { Point, Shape, VisualShape } from "./geometry.js";
+import { LabeledPoints, Point, Shape, VisualShape } from "./geometry.js";
 import { convert_to_shape, convert_to_strings, convert_to_labeled_points } from "./stringify.js";
 import { Runner } from "./runner.js";
 import { create_solver, Event, PointInspector, Setter, Solver }from "./solver.js";
 import { bindToggleButton, makeBrowserRenderer } from "./browserui.js";
+import worker from "node:worker_threads";
+import { fileURLToPath } from 'url';
 
 const vshapes: VisualShape[] = [
     {   // tetra I
@@ -99,57 +101,136 @@ const shapes = vshapes.map(convert_to_shape);
 const labeledPoints = convert_to_labeled_points(vboard, 4);
 const board_points = labeledPoints.map(e => e.point);
 
-if (typeof window !== "object") {
-    const args = new Set(process.argv);
-    const picked = labeledPoints.filter((lp) => args.has(lp.label));
-    if (picked.length <= 3) {
-        const start = performance.now();
+type WorkerTask = {
+    id: number,
+    placed: Point[],
+    picked: Point[],
+    shapes: Shape[],
+    board_points: Point[]
+};
 
-        console.log("Picked: " + picked.map((lp) => lp.label).join(", "));
-        const solver:Solver = create_solver(board_points, shapes, (set:Setter, pi:PointInspector) => {
-            picked.forEach((lp) => set(lp.point, "X"));
-    
-            console.log("Solving for:");
-            logBoard(pi);
-        });
-    
-        let done = false;
-        let counter = 0;
-        const stepping = 1e5;
-    
-        function logBoard(pi:PointInspector) {
-            console.log(convert_to_strings(board_points, (p) => pi(p) || ".").join('\n'));
+type Solution = {
+    id: number,
+    text: string,
+    board: LabeledPoints
+};
+
+if (typeof worker !== undefined) {
+    function toString(pi:PointInspector) {
+        return convert_to_strings(board_points, (p) => pi(p) || ".").join('\n')
+    }
+
+    function logBoard(pi:PointInspector) {
+        console.log(toString(pi));
+    }
+
+    const verbose = false;
+
+    if (worker.isMainThread) {
+        const args = new Set(process.argv);
+        const picked = labeledPoints.filter((lp) => args.has(lp.label));
+        if (picked.length <= 3) {
+            const start = performance.now();
+
+            console.log("Picked: " + picked.map((lp) => lp.label).join(", "));
+            const solver:Solver = create_solver(board_points, shapes.slice(0, 1), (set:Setter, pi:PointInspector) => {
+                picked.forEach((lp) => set(lp.point, "X"));
+        
+                console.log("Solving for:");
+                logBoard(pi);
+            });
+
+            let counter = 0;
+            const stepping = 1e5;
+        
+            let id = 0;
+            const remainingShapes = shapes.slice(1);
+
+            const callback = (pi:PointInspector, e:Event) => {
+                if (e.kind === "solved") {
+                    if (verbose) {
+                        console.log("solution:");
+                        logBoard(pi);
+                        console.log("elapsed time: " + ((performance.now() - start) / 1000));
+                    }
+                    const piece = board_points.filter(p => pi(p) === '0');
+
+                    const task:WorkerTask = {
+                        id: id++,
+                        placed: piece,
+                        picked: picked.map(lp => lp.point),
+                        shapes: remainingShapes,
+                        board_points: board_points
+                    };
+
+                    const w = new worker.Worker(fileURLToPath(import.meta.url), {
+                        workerData: task
+                    });
+
+                    w.on("message", (sln:Solution) => {
+                        console.log("Worker: " + sln.id + "\n" + sln.text + "\n");
+                        // process.exit(0);
+                    });
+                } else if (verbose && e.kind === "failed") {
+                    console.log("failed to place: ");
+                    console.log(convert_to_strings(e.shape.points, (p) => "O").join('\n'));
+                    console.log("into");
+                    logBoard(pi);
+                    console.log("--------")
+                }
+        
+                counter++;
+        
+                if (counter % stepping === 0) {
+                    console.log(counter / stepping);
+                }
+            }
+        
+            while(solver(callback)) { }
+
+            if (verbose) {
+                console.log("done spawning workers");
+            }
+
+        } else {
+            console.log("Too many arguments (should be 3 or fewer)");
+            process.exit(1);
         }
-    
-        const verbose = false;
-    
+    } else {
+        const task = worker.workerData as WorkerTask;
+        if (verbose) {
+            console.log("in Worker: " + task.id);
+        }
+        const solver:Solver = create_solver(task.board_points, task.shapes, (set:Setter, pi:PointInspector) => {
+            task.picked.forEach((p) => set(p, "X"));
+            task.placed.forEach((p) => set(p, task.shapes.length + ""));
+
+            if (verbose) {
+                console.log("Solving for:");
+                logBoard(pi);
+            }
+        });
+
         const callback = (pi:PointInspector, e:Event) => {
             if (e.kind === "solved") {
-                console.log("solution:");
-                logBoard(pi);
-                console.log("elapsed time: " + ((performance.now() - start) / 1000));
-                done = true;
-            } else if (verbose && e.kind === "failed") {
-                console.log("failed to place: ");
-                console.log(convert_to_strings(e.shape.points, (p) => "O").join('\n'));
-                console.log("into");
-                logBoard(pi);
-                console.log("--------")
+                const board = task.board_points.map((p) => { return { label: pi(p) || "", point: p}});
+
+                const sln:Solution = {
+                    id: task.id,
+                    text: toString(pi),
+                    board: board
+                };
+                worker.parentPort?.postMessage(sln);
             }
-    
-            counter++;
-    
-            if (counter % stepping === 0) {
-                console.log(counter / stepping);
-            }
+        };
+
+        while(solver(callback)) { }
+
+        if (verbose) {
+            console.log("Worker " + task.id + " finished.");
         }
-    
-        while(!done && solver(callback)) { }
-    } else {
-        console.log("Too many arguments (should be 3 or fewer)");
-        process.exit(1);
     }
-} else {
+} else {    
     let render:(ps:PointInspector) => void = () => {};
     let solver:Solver | undefined;
     const state = new Runner();
